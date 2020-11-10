4c743319823ce8ae7ac4db5c7113053010a9f31d
j <longnguyen16.ck27@st.ueh.edu.vn>
2020-11-10 10:00:51 +0700

    Done lession 18-1

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState, Fragment } from "react";
+import Modal from "./components/Modal";
+import "./App.css";
+
+export default function App() {
+  const [isDisplay, setIsDisplay] = useState("none");
+
+  const [content, setContent] = useState("Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamcolaboris nisi ut aliquip ex ea commodo consequat. Duis auteirure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.");
+
+  function handleDisplay() {
+    setIsDisplay((isDisplay) => isDisplay === "none" ? "block" : "none");
+  }
+
+  return (
+    <Fragment>
+      <Modal isDisplay={isDisplay} handleDisplay={handleDisplay} content={content}/>
+        <button type="button" class="btn btn-success" onClick={handleDisplay}>
+          Open Modal
+        </button>
+    </Fragment>
+  );
+}
---
+++ src/components/Modal.js
@@ -0,0 +1,53 @@
+import React, { Fragment } from "react";
+import "./Modal.css";
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      <div id="overlay" style={{ display: props.isDisplay }}>
+        <div id="modal">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title">This is Modal 1</h5>
+                <button
+                  type="button"
+                  class="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                  onClick={props.handleDisplay}
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <p>
+                  {props.content}
+                </p>
+              </div>
+              <div class="modal-footer">
+                <button
+                  type="button"
+                  class="btn btn-success"
+                  data-dismiss="modal"
+                  onClick={props.handleDisplay}
+                >
+                  Accept
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-danger"
+                  onClick={props.handleDisplay}
+                >
+                  Decline
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Modal;
